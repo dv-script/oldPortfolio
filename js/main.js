321c7f8098bc415f850b62e5d5f947efd985dae2
Davi Hasson Castro <davihasson@gmail.com>
2022-08-26 17:53:03 -0300

    update portfolio

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const swiper = new Swiper('.projects__container', {
+  loop: true,
+  simulateTouch: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// ========================================================================
+
+const toTop = document.querySelector(".to-top__button");
+
+window.addEventListener("scroll", function (el) {
+  if (window.scrollY > 946) {
+    toTop.classList.add("visible");
+    toTop.classList.remove("invisible")
+  } else {
+    toTop.classList.remove("visible");
+    toTop.classList.add("invisible");
+  }
+});
